44e81083d34b27bfe882ca22a05df4775b32f19a
kikk <kikpanda@gmail.com>
2019-11-20 00:46:48 +0700

    show edit and delete button for post's owner

--- app/src/main/java/com/example/egci428_travelguide/Activity/PlaceInfoActivity.kt
+++ app/src/main/java/com/example/egci428_travelguide/Activity/PlaceInfoActivity.kt
@@ -3,12 +3,16 @@ package com.example.egci428_travelguide.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
 import com.example.egci428_travelguide.DataModel.Place
 import com.example.egci428_travelguide.DataModel.PlaceInfo
 import com.example.egci428_travelguide.DataSource.ProvincePlacesDataSource
 import com.example.egci428_travelguide.R
 import com.example.egci428_travelguide.UserInfo
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 import com.google.firebase.storage.FirebaseStorage
 import com.squareup.picasso.Picasso
@@ -17,6 +21,8 @@ import kotlinx.android.synthetic.main.activity_profile.*
 import kotlinx.android.synthetic.main.activity_province_places.*
 
 class PlaceInfoActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var currentUser:FirebaseUser
     lateinit var dataReference: DatabaseReference
     var province = ""
     var place = ""
@@ -29,12 +35,30 @@ class PlaceInfoActivity : AppCompatActivity() {
             place = data.getString("place")!!
         }
         placeName.setText(place)
+        //check current user
+        auth = FirebaseAuth.getInstance()
+        currentUser = auth.currentUser!!
+
         //fetch data from database
         dataReference = FirebaseDatabase.getInstance().getReference("province/$province/place/$place/info")
         val dataListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 val placeData = dataSnapshot.getValue(PlaceInfo::class.java)
                 updateData(placeData!!)
+                if(placeData.uid == currentUser!!.uid){//current user is the post owner
+                    val editbtnView = findViewById(R.id.editBtn) as View
+                    val deletebtnView = findViewById(R.id.deleteBtn) as View
+                    editbtnView.visibility = View.VISIBLE
+                    deletebtnView.visibility = View.VISIBLE
+                }else{
+                    val placenameView = findViewById(R.id.placeName) as View
+                    placenameView.setLayoutParams(
+                        LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT
+                        )
+                    )
+                }
                 val userRef = FirebaseDatabase.getInstance().getReference("Users/"+placeData.uid)
                 val userDataListener = object : ValueEventListener {
                     override fun onDataChange(dataSnapshot: DataSnapshot) {
