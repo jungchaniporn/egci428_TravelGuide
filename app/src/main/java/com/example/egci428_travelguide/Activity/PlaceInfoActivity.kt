0e3a7b64c606de07fe6ab20b68fb3c4d76daa8a7
kikk <kikpanda@gmail.com>
2019-11-19 23:53:01 +0700

    show post's owner profile below information

--- app/src/main/java/com/example/egci428_travelguide/Activity/PlaceInfoActivity.kt
+++ app/src/main/java/com/example/egci428_travelguide/Activity/PlaceInfoActivity.kt
@@ -2,12 +2,18 @@ package com.example.egci428_travelguide.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 import com.example.egci428_travelguide.DataModel.Place
 import com.example.egci428_travelguide.DataModel.PlaceInfo
 import com.example.egci428_travelguide.DataSource.ProvincePlacesDataSource
 import com.example.egci428_travelguide.R
+import com.example.egci428_travelguide.UserInfo
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_place_info.*
+import kotlinx.android.synthetic.main.activity_profile.*
 import kotlinx.android.synthetic.main.activity_province_places.*
 
 class PlaceInfoActivity : AppCompatActivity() {
@@ -29,6 +35,29 @@ class PlaceInfoActivity : AppCompatActivity() {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 val placeData = dataSnapshot.getValue(PlaceInfo::class.java)
                 updateData(placeData!!)
+                val userRef = FirebaseDatabase.getInstance().getReference("Users/"+placeData.uid)
+                val userDataListener = object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        val userData = dataSnapshot.getValue(UserInfo::class.java)
+                        usernameInfo.setText(userData!!.username)
+                        val userimgView = findViewById(R.id.userAvatar) as ImageView
+                        val storageReference = FirebaseStorage.getInstance().reference
+                        storageReference!!.child(userData!!.imageRef).downloadUrl.addOnSuccessListener {
+                            Picasso
+                                .get()
+                                .load(it)
+                                .into(userimgView);
+                        }.addOnFailureListener {
+                            // Handle any errors
+                        }
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+                        // Getting Post failed, log a message
+                        println(databaseError)
+                    }
+                }
+                userRef.addListenerForSingleValueEvent(userDataListener)
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
